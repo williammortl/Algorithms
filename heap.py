29587c20d36dc50c903e94c27697cb637b30d9ec
William Mortl <will@williammortl.com>
2015-02-16 10:44:57 -0700

    updated heap and heap sort

--- heap.py
+++ heap.py
@@ -22,8 +22,7 @@ def right(list, i):
 	return (2 * i) + 2
 
 # heapify with a comparator, can build max or min heaps
-def heapify(list, i, comparator):
-	s = len(list)
+def heapify(list, i, comparator, s):
 	l = left(list, i)
 	r = right(list, i)
 	if ((l < s) and (comparator(list[l], list[i]))):
@@ -36,14 +35,15 @@ def heapify(list, i, comparator):
 		tmp = list[i]
 		list[i] = list[maxmin]
 		list[maxmin] = tmp
-		heapify(list, maxmin, comparator)
+		heapify(list, maxmin, comparator, s)
 	return list
 
 # converts a list to a heap
-def buildHeap(list, maxOrMin):
-	s = (len(list) / 2) - 1
-	for i in xrange(s, -1, -1):
-		heapify(list, i, maxOrMin)
+def buildHeap(list, comparator):
+	s = len(list)
+	midpoint = (len(list) / 2) - 1
+	for i in xrange(midpoint, -1, -1):
+		heapify(list, i, comparator, s)
 	return list
 
 # a maximum heap comparator
@@ -74,7 +74,7 @@ def printHeap(list):
 if __name__ == "__main__":
 	if (len(sys.argv) < 3):
 		print("\r\nHeap Algorithms by William M Mortl")
-		print("Usage: python heap.py {+ for max | - for min} \"{comma seperated list of values to sort}\"")
+		print("Usage: python heap.py {+ for max | - for min} \"{comma seperated list of values to make into a heap}\"")
 		print("Example: python heap.py + \"9,111,2,31,1,0\"\r\n")
 	else:
 		maxOrMin = "Min" if sys.argv[1].strip() == "-" else "Max"
---
+++ heapSort.py
@@ -0,0 +1,35 @@
+# Heap Sort Algorithm
+# Implemented by William M Mortl
+
+# imports
+import sys
+from random import randint
+import mergeSort
+import heap
+
+# heap sort, use maxComparator for ascending and minComparator for descending
+def heapSort(list, i, j):
+	list = heap.buildHeap(list, heap.maxComparator)
+	s = len(list)
+	for i in xrange((s - 1), 0, -1):
+		tmp = list[0]
+		list[0] = list[i]
+		list[i] = tmp
+		s = s - 1
+		heap.heapify(list, 0, heap.maxComparator, s)
+	return list
+
+# main entry point
+if __name__ == "__main__":
+	if (len(sys.argv) < 2):
+		print("\r\nHeap Sort by William M Mortl")
+		print("Usage: python heapSort.py \"{comma seperated list of values to sort}\"")
+		print("Example: python heapSort.py \"9,111,2,31,1,0\"\r\n")
+	else:
+		listToSort = map(int, sys.argv[1].split(","))
+		print(("\r\nSorting:\r\n%s") % str(listToSort))
+		mergeSorted = mergeSort.mergeSort(listToSort)
+		heapSorted = heapSort(listToSort, 0, len(listToSort) - 1)
+		print(("Merge Sorted list:\r\n%s") % str(mergeSorted))
+		print(("Heap Sorted list:\r\n%s") % str(heapSorted))
+		print(("Lists equal? %s\r\n") % str(mergeSorted == heapSorted))
